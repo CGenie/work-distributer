eb47fdef0a4ace83f280491376a1098d5ccf99a1
Przemek Kaminski <cgenie@gmail.com>
2014-04-15 23:27:08 +0200

    datetime-enabled SourceFile, serveFile function

--- src/Main.hs
+++ src/Main.hs
@@ -1,11 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 module Main where
 
+import Data.List (sortBy)
 import qualified Data.ConfigFile as ConfigFile
 import Data.ByteString.Lazy.Char8 (pack)
 import Data.Either.Utils (forceEither)
 import Data.Digest.Pure.SHA (sha256, showDigest)
 import System.Directory (getHomeDirectory, createDirectoryIfMissing, getDirectoryContents)
+
+import Data.DateTime (getCurrentTime, DateTime)
 
 import           Control.Applicative
 import           Snap.Core
@@ -14,8 +17,15 @@ import           Snap.Http.Server
 
 type SourceDir = String
 type DestinationDir = String
+
+type AppConfig = (SourceDir, DestinationDir)
+
 type Hash = String
-data SourceFileHashed = SourceFileHashed (FilePath, Hash)
+
+data SourceFileHashed = SourceFileHashed FilePath Hash
+   deriving (Show, Eq)
+
+data SourceFile = SourceFile SourceFileHashed DateTime
    deriving (Show, Eq)
 
 data Email = EmailConfirmed String | EmailUnconfirmed String
@@ -27,19 +37,21 @@ data User = User {
 main :: IO ()
 main = do
     (source, target) <- readConfig
-    sourceFiles <- getSourceFiles source
-    sourceFilesHashed <- mapM computeFileHash sourceFiles
+    sourceFiles' <- getSourceFiles source
+    sourceFilesHashed <- mapM computeFileHash sourceFiles'
+    now <- getCurrentTime
+    let sourceFiles = map (\sf -> SourceFile sf now) sourceFilesHashed
 
     putStrLn "Your watched files:"
-    mapM_ (putStrLn . show) sourceFilesHashed
+    mapM_ (putStrLn . show) sourceFiles
 
     createDirectoryIfMissing True target
 
     users <- readUsers
 
-    quickHttpServe site
+    quickHttpServe $ site (source, target) sourceFiles
 
-readConfig :: IO (SourceDir, DestinationDir)
+readConfig :: IO AppConfig
 readConfig = do
     home <- getHomeDirectory
     val <- ConfigFile.readfile ConfigFile.emptyCP (home ++ "/.work-distributer")
@@ -85,10 +97,10 @@ computeFileHash filePath = do
 computeFileContentHash :: FilePath -> String -> SourceFileHashed
 computeFileContentHash filePath contents = 
         let hash = showDigest $ sha256 $ pack $ filePath ++ contents
-        in SourceFileHashed (filePath, hash)
+        in SourceFileHashed filePath hash
 
-site :: Snap ()
-site =
+site :: AppConfig -> [SourceFile] -> Snap ()
+site config sourceFilesHashed =
     ifTop (writeBS "hello world") <|>
     route [ ("foo", writeBS "bar")
           , ("echo/:echoparam", echoHandler)
@@ -100,3 +112,14 @@ echoHandler = do
     param <- getParam "echoparam"
     maybe (writeBS "must specify echo/param in URL")
           writeBS param
+
+
+serveFile :: [SourceFile] -> Snap (Maybe SourceFile)
+serveFile [] = return Nothing
+serveFile sourceFiles = do
+    let sf = last $ sortBy (\a b -> compare (sourceFileDateTime a) (sourceFileDateTime b)) sourceFiles
+    return $ Just sf
+
+
+sourceFileDateTime :: SourceFile -> DateTime
+sourceFileDateTime (SourceFile _ dt) = dt
